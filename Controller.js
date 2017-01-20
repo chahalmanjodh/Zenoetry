1fd18fe30d4c8537280ac7e8736518f9e9110d01
chahalmanjodh <chahal.manjodh@gmail.com>
2017-01-20 10:45:08 -0800

    Add files via upload

---
+++ Controller.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('Zen', [])
+.controller('MainCtl', function($scope){
+        $scope.list = [
+            {name:'Scott'},
+            {name:'Scott'},
+            {name:'Scott'},
+            {name:'Scott'},
+            {name:'Scott'},
+            {name:'Scott'},
+        ]
+
+    });
